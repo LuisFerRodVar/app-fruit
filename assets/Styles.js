59a95f7646392b4424c1d06e3382b3965084d12c
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-06-14 18:38:51 -0600

    Added login code

---
+++ assets/Styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+const Styles = StyleSheet.create({
+
+  logoContainer: {
+    position: 'absolute',
+    top: 100,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+
+});
+export default Styles;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Styles from "../assets/Styles";
 
-export default function Login() {
+export default function Login({navigation}) {
   return (
     <View>
-      <Text>Login</Text>
+      <View style={Styles.logoContainer}>
+        <Image style={Styles.logo} source={require('../assets/logo_fruit.png')} />
+      </View>
+      <TextInput style={Styles.input} placeholder="correo electronico" />
+      <TextInput style={Styles.input} placeholder="contraseña" secureTextEntry={true} />
+
+      <TouchableOpacity style={Styles.button} onPress={() => {navigation.navigate("add")}}>
+        <Text style={Styles.buttonText}>Iniciar sesión</Text>
+      </TouchableOpacity>
+
+      <View style={Styles.signupTextCont}>
+        <Text style={Styles.signupText}>Crear una cuenta nueva</Text>
+        <TouchableOpacity onPress={() => {navigation.navigate("register")}}>
+          <Text style={Styles.signupButton}>Regístrate</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={Styles.signupButton}>Olvidó contraseña?</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
