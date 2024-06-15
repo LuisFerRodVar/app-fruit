505c323c21f62ba663d805c41d1ee1c2e0652dc5
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-06-14 18:50:25 -0600

    Added components

--- src/Add.jsx
+++ src/Add.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
-
+import { View, Text, Image , TextInput, TouchableOpacity} from 'react-native';
+import Styles from "../assets/Styles";
 
 export default function Add() {
   return (
     <View>
-      <Text>Add</Text>
+      <View style={Styles.logoContainer}>
+        <Image style={Styles.logo} source={require('../assets/logo_fruit.png')} />
+      </View>
+      <View style={Styles.formContainer}>
+        <Text style={Styles.title}>Agregar producto</Text>
+        <TextInput style={Styles.input} placeholder="Nombre producto" />
+        <TextInput style={Styles.input} placeholder="Código Producto" />
+        <TextInput style={Styles.input} placeholder="Cantidad" />
+        <TextInput style={Styles.input} placeholder="Fecha caducidad" />
+        <TouchableOpacity style={Styles.button}>
+          <Text style={Styles.buttonText}>Agregar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity} from 'react-native'
 import React from 'react'
+import Styles from "../assets/Styles";
 
-export default function Register() {
+export default function Register({navigation}) {
   return (
     <View>
-      <Text>Register</Text>
+      <View style={Styles.logoContainer}>
+        <Image style={Styles.logo} source={require('../assets/logo_fruit.png')} />
+      </View>
+      <View style={Styles.formContainer}>
+        <Text style={Styles.title}>Crear cuenta nueva</Text>
+        <TextInput style={Styles.input} placeholder="Nombre Completo" />
+        <TextInput style={Styles.input} placeholder="Correo electronico" />
+        <TextInput style={Styles.input} placeholder="Contraseña" secureTextEntry={true} />
+        <TextInput style={Styles.input} placeholder="Comprobar contraseña" secureTextEntry={true} />
+        <TouchableOpacity style={Styles.button} onPress={() => {navigation.navigate("login")}}>
+          <Text style={Styles.buttonText}>Registrarse</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
